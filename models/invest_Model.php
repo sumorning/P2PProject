f3386b71ad51258b583f73b65397fcf4fcb3e948
Chronicle <smus0414@gmail.com>
2018-08-01 19:39:11 +0900

    Commit1
    Commit1

--- models/invest_Model.php
+++ models/invest_Model.php
@@ -23,42 +23,42 @@ class Invest_Model extends CI_Model
     }
     public function getInvestByCId($CId, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where cid=' . $CId . ' order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where cid=' . $CId . ' order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
     public function getInvestByPId($PId, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where pid =' . $PId . ' order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where pid =' . $PId . ' order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 //
     public function getInvestByInRate($UpperLimit, $LowerLimit, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where InRate >=' . $LowerLimit . ' and ' . $UpperLimit . '<= InRate order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where InRate >=' . $LowerLimit . ' and ' . $UpperLimit . '<= InRate order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 //
     public function getInvestByTotFund($UpperLimit, $LowerLimit, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where totFund >=' . $LowerLimit . ' and ' . $UpperLimit . '<= InRate order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where totFund >=' . $LowerLimit . ' and ' . $UpperLimit . '<= InRate order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 
     public function getInvestBySId($SId, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where sid =' . $SId . ' order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where sid =' . $SId . ' order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 
     public function getInvestByPeriod($UpperLimit, $LowerLimit, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where period <=' . $UpperLimit . ' and period >= ' . $LowerLimit . ' order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where period <=' . $UpperLimit . ' and period >= ' . $LowerLimit . ' order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 
     public function getInvestByIName($SearchName, $Start, $Num)
     {
-        $Query = 'SELECT * from Invest where iName LIKE "%' . $SearchName . '%" order by open desc Limit ' . $Start . ',' . $Num;
+        $Query = 'SELECT * from invest where iName LIKE "%' . $SearchName . '%" order by open desc Limit ' . $Start . ',' . $Num;
         return $this->db->query($Query)->result();
     }
 
